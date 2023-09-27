f08512f8b7b21e2c7b7e6bc07737dc5982570f3e
Gayatri <63839136+gayatri-22@users.noreply.github.com>
2023-09-27 12:54:42 +0530

    Update 0075-sort-colors.java

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,8 @@
 class Solution {
     public void sortColors(int[] nums) {
      //   Arrays.sort(nums);
+
+--------------------------------------------
         int zerofreq=0;
         int onefreq=0;
         int twofreq=0;
@@ -33,3 +35,29 @@ class Solution {
    
     }
 }
+--------------------------------------------
+
+    DUTCH NATIONAL FLAG ALGORITHM
+
+    int low=0, mid=0,high = nums.length-1;
+     while(mid<=high){
+         if(nums[mid]==0){
+             swap(nums,low,mid);
+             low++;
+             mid++;
+         }else if (nums[mid]==1){
+             mid++;
+         }
+         else{
+             swap(nums,mid,high);
+             mid++;
+             high--;
+         }
+     }
+   
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j]= temp;
