c416fb63f5e9e74962cc4ef941a622bfe1a889fc
Gayatri <63839136+gayatri-22@users.noreply.github.com>
2023-09-27 11:40:46 +0530

    Time: 0 ms (100%), Space: 40.6 MB (94.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,35 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+     //   Arrays.sort(nums);
+        int zerofreq=0;
+        int onefreq=0;
+        int twofreq=0;
+
+     for(int i=0;i<nums.length;i++){
+         if(nums[i]==0){
+             zerofreq++;
+         }
+
+         else if(nums[i]==1){
+             onefreq++;
+         }
+
+         else if(nums[i]==2){
+             twofreq++;
+         }
+     }
+
+     for(int i=0;i<zerofreq;i++){
+            nums[i]=0;
+     }
+
+     for(int i=zerofreq;i<zerofreq+onefreq;i++){
+         nums[i]=1;
+     }
+
+     for(int i=zerofreq+onefreq;i<zerofreq+onefreq+twofreq;i++){
+         nums[i]=2;
+     }
+   
     }
 }
